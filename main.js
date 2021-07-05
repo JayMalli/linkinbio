0787ab99f0400191c9c6b5c8e7d6251721310b11
JayMalli <jaymalli747@gmail.com>
2021-07-05 16:25:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+db.collection("posts")
+  .orderBy("createdAt", "desc")
+  .onSnapshot((snap) => {
+    container.innerHTML = "";
+    snap.forEach((doc) => {
+      let post = doc.data();
+      container.innerHTML += `
+			<a class="post-div" href=${post.link}  target="_blank">
+				<img src=${post.url} loading="lazy" alt="project image" />
+			</a>
+			`;
+    });
+  });
